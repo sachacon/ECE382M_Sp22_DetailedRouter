b1f61ad187212b25c2746cdc725cc2f5a9b9bf5c
zhxhao007 <90884985+zhxhao007@users.noreply.github.com>
2022-04-16 20:57:07 -0500

    Update comments

--- A_Star_Search/student_impl/eid_xz7622.py
+++ A_Star_Search/student_impl/eid_xz7622.py
@@ -35,7 +35,6 @@ class A_Star_Search(A_Star_Search_Base):
         """
         # TODO initialize any auxiliary data structure you need
 
-        self.visited_node_count = 0
         self.source_node = (self.pin_pos_x[0], self.pin_pos_y[0]) # the first node in all nodes as the source
 
         self.source_node_array = np.ndarray(shape = (1, 2))
@@ -50,19 +49,6 @@ class A_Star_Search(A_Star_Search_Base):
         self.sink_node_array_list = self.sink_node_array.tolist()
         # print(self.sink_node_array_list)
         # print("sink_node_array:", self.sink_node_array)
-        
-        # p1 = np.ndarray(shape = (1, 2))
-        # p1[0][0] = 0
-        # p1[0][1] = 8
-        # print(p1 in self.sink_node_array)
-
-        # self.pin_positions = np.append(self.pin_positions, p1, axis = 0)
-       
-        # print(self._find_nearest_target_dist(p1, self.pin_positions))
-
-        # p2 = GridAstarNode()
-        # p2.pos = self.source_node
-        # print(self.compute_f_array(p2, p1))
 
         self.sink_node = (self.pin_pos_x[1], self.pin_pos_y[1])
 
@@ -140,34 +126,13 @@ class A_Star_Search(A_Star_Search_Base):
         # To make this method clean, you can extract subroutines as methods of this class
         # But do not override methods in the parent class
         # Please strictly follow the return type requirement.
-
-        # node_path = PriorityQueue() # open list for nodes to be visited, green
-        # node_record = {} # keep track of visited nodes, green node number
-
-        # source_node = GridAstarNode()
-        # source_node.pos = self.source_node
-        # source_node.cost_g = self.compute_g(source_node)
-        # # source_node.cost_f = self.compute_f(source_node)
-        # source_node.cost_f = self.compute_f_array(source_node, self.sink_node_array)
-        # source_node.bend_count = self.bend_count_number(source_node)
-        # source_node.visited = True ##
-
-        # node_path.put(source_node) # add source node to open list
-        # node_record[source_node.pos] = source_node
         
-        sink_node = GridAstarNode()
-        # sink_node.pos = self.sink_node
-
-        # path = [] # closed list for nodes explored, red
-        path_list = []
-
+        sink_node = GridAstarNode() # initialize class
+        path_list = [] # record path to output
         sink_node_array_as_list = self.sink_node_array.tolist()
-
-        wirelength_list = []
-
-        visited_node_list = []
-
-        connect_net = np.ndarray(shape = (0, 2))
+        wirelength_list = [] # output
+        visited_node_list = [] # output
+        connect_net = np.ndarray(shape = (0, 2)) # record routed paths
 
         for j in range(0, self.n_pins - 1):
             node_path = PriorityQueue() # open list for nodes to be visited, green
@@ -182,7 +147,6 @@ class A_Star_Search(A_Star_Search_Base):
                 source_node = GridAstarNode()
                 source_node.pos = self.source_node
                 source_node.cost_g = self.compute_g(source_node)
-                # source_node.cost_f = self.compute_f(source_node)
                 source_node.cost_f = self.compute_f_array(source_node, self.sink_node_array)
                 source_node.bend_count = self.bend_count_number(source_node)
                 source_node.visited = True ##
@@ -194,19 +158,12 @@ class A_Star_Search(A_Star_Search_Base):
                     node = node_path.get() # get a priority node from queue based on f(n)
 
                     if node.pos not in path:
-                        # self.visited_node_count += 1
                         visited_node += 1
 
                     path.append(node.pos) # node is explored, red
 
                     node_pos_as_list = list(node.pos) # node position: tuple to list
-                    # connect_net = np.append(connect_net, [node_pos_as_list], axis = 0)
-
-                    # if node.pos == sink_node.pos: # if popped out node is sink node, done
-                    #     sink_node = node
-                    #     break
-                    # node_pos_as_list = list(node.pos) # node position: tuple to list
-                    # sink_node_array_as_list = self.sink_node_array.tolist()
+                    
                     if node_pos_as_list in sink_node_array_as_list: # check if this node is in the sink array -> if this node is one of the sink nodes
                         # print("sink node:", node_pos_as_list)
                         # print("backtrack:", self._backtrack(node))
@@ -219,9 +176,6 @@ class A_Star_Search(A_Star_Search_Base):
                             connect_net = np.append(connect_net, [list(tracknode[k])], axis = 0)
 
                         break
-                        # if len(self.sink_node_array) == 0: # if all sink nodes have been visited and connected with source
-                        #     sink_node = node
-                        #     break
 
                     self.find_neighbors(node) # find neighbors of the current node
                     for i in range(0, len(node.neighbors)): # for each neighbor
@@ -238,7 +192,6 @@ class A_Star_Search(A_Star_Search_Base):
                                 neighbor_copy.parent = node
                                 neighbor_copy.bend_count = self.bend_count_number(neighbor_copy)
                                 neighbor_copy.cost_g = self.compute_g(neighbor_copy)
-                                # neighbor_copy.cost_f = self.compute_f(neighbor_copy)
                                 neighbor_copy.cost_f = self.compute_f_array(neighbor_copy, self.sink_node_array)
 
                                 if neighbor_copy.cost_g < node_record[neighbor.pos].cost_g: # if potentially a smaller cost_g
@@ -266,8 +219,7 @@ class A_Star_Search(A_Star_Search_Base):
                                 node_record[neighbor.pos] = neighbor
 
             else: # sink nodes as the source to connect to connected net
-                # source_node = GridAstarNode()
-
+                # find the closest sink node to routed path
                 distance_sink_to_connect_net = []
                 left_sink_list = self.sink_node_array.tolist()
                 # print("left sink list:", left_sink_list)
@@ -278,9 +230,6 @@ class A_Star_Search(A_Star_Search_Base):
                 # print("left sink distance:", distance_sink_to_connect_net)
                 close_node = distance_sink_to_connect_net.index(min(distance_sink_to_connect_net))
 
-
-                # if sink_node_array_as_list[j - 1] in connect_net.tolist():
-                #     continue
                 source_node = GridAstarNode()
                 source_node.pos = (int(self.sink_node_array[close_node][0]), int(self.sink_node_array[close_node][1]))
                 # print("? node pos:", source_node.pos)
@@ -298,28 +247,17 @@ class A_Star_Search(A_Star_Search_Base):
                     # print("node pos:", node.pos)
 
                     if node.pos not in path:
-                        # self.visited_node_count += 1
                         visited_node += 1
 
                     path.append(node.pos) # node is explored, red
 
                     node_pos_as_list = list(node.pos) # node position: tuple to list
 
-                    # connect_net = np.append(connect_net, node_pos_as_list, axis = 0)
-
-                    # if node.pos == sink_node.pos: # if popped out node is sink node, done
-                    #     sink_node = node
-                    #     break
-                    # node_pos_as_list = list(node.pos) # node position: tuple to list
-                    # sink_node_array_as_list = self.sink_node_array.tolist()
-
                     # print("connect net:", connect_net.tolist())
                     if node_pos_as_list in connect_net.tolist(): # check if this node is in the connect net -> if source node has been connected to the net
                         # print("sink node:", node_pos_as_list)
                         # print("backtrack:", self._backtrack(node))
-                        # self.sink_node_array = np.delete(self.sink_node_array, self.sink_node_array.tolist().index(sink_node_array_as_list[j - 1]), axis = 0) # remove the connected sink node from sink node arrays
                         self.sink_node_array = np.delete(self.sink_node_array, close_node, axis = 0) # remove the connected sink node from sink node arrays
-                        
 
                         sink_node = node
                         tracknode = self._backtrack(sink_node)
@@ -328,9 +266,6 @@ class A_Star_Search(A_Star_Search_Base):
                             connect_net = np.append(connect_net, [list(tracknode[k])], axis = 0)
                         
                         break
-                        # if len(self.sink_node_array) == 0: # if all sink nodes have been visited and connected with source
-                        #     sink_node = node
-                        #     break
 
                     self.find_neighbors(node) # find neighbors of the current node
                     for i in range(0, len(node.neighbors)): # for each neighbor
@@ -347,7 +282,6 @@ class A_Star_Search(A_Star_Search_Base):
                                 neighbor_copy.parent = node
                                 neighbor_copy.bend_count = self.bend_count_number(neighbor_copy)
                                 neighbor_copy.cost_g = self.compute_g(neighbor_copy)
-                                # neighbor_copy.cost_f = self.compute_f(neighbor_copy)
                                 neighbor_copy.cost_f = self.compute_f_array(neighbor_copy, connect_net)
 
                                 if neighbor_copy.cost_g < node_record[neighbor.pos].cost_g: # if potentially a smaller cost_g
@@ -368,40 +302,41 @@ class A_Star_Search(A_Star_Search_Base):
                                 neighbor.parent = node
                                 neighbor.bend_count = self.bend_count_number(neighbor)
                                 neighbor.cost_g = self.compute_g(neighbor)
-                                # neighbor.cost_f = self.compute_f(neighbor)
                                 neighbor.cost_f = self.compute_f_array(neighbor, connect_net)
                                 neighbor.visited = True
                                 node_path.put(neighbor) # add neighbor to open list, green
                                 node_record[neighbor.pos] = neighbor
             
+            # path_list as point_wise output
             # if j == 0:
-            #     # tracknode = self._backtrack(sink_node)
             #     for k in range(len(tracknode)):
             #         path_list.append(tracknode[k])
             #     wirelength_list.append(len(tracknode) - 1)
+            # else:
+            #     tracknode.reverse()
+            #     for k in range(len(tracknode)):
+            #         path_list.append(tracknode[k])
+            #     wirelength_list.append(len(tracknode) - 1)
+
             if j != 0:
                 tracknode.reverse()
 
-            # for k in range(len(tracknode)):
-            #     path_list.append(tracknode[k])
             for k in range(len(self._merge_path(tracknode))):
-                path_list.append(self._merge_path(tracknode)[k])
+                path_list.append(self._merge_path(tracknode)[k]) # path_list as vector-wise output
             wirelength_list.append(len(tracknode) - 1)
+
             # print("tracknode: ", tracknode)
             # print("merge tracknode: ", self._merge_path(tracknode))
             
 
             visited_node_list.append(visited_node)
 
-                # path_list.append(tracknode)
-
-        # path_list = self._backtrack(sink_node)
-        # wirelength = len(path_list) - 1
-        wirelength = sum(wirelength_list)
+        # wirelength = len(path_list) - 1 # point-wise
+        wirelength = sum(wirelength_list) # vector-wise
 
         # print(connect_net)
         # print("path list:", path_list)
         # print("merge path:", self._merge_path(path_list))
 
-        # return (self._merge_path(path_list), wirelength, wirelength_list, visited_node_list)
-        return (path_list, wirelength, wirelength_list, visited_node_list)
+        # return (self._merge_path(path_list), wirelength, wirelength_list, visited_node_list) # point-wise
+        return (path_list, wirelength, wirelength_list, visited_node_list) # vector-wise
